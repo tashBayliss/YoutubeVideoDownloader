50baa36c436f7daf2240ededa76f9601e8fec756
Tash <psynb2@exmail.nottingham.ac.uk>
2020-06-18 18:39:28 +0100

    gui finished

--- YoutubeDownloader.py
+++ YoutubeDownloader.py
@@ -1,9 +1,25 @@
 # Youtube video downloader
 
-import tkinter as tk
+import tkinter
 
-root = tk.Tk()
+def download():
+    global entry1
+    toDownload = entry1.get()
+    print(toDownload)
 
-title = tk.Label(root, text="Youtube Video Downloader")
+
+
+root = tkinter.Tk()
+
+title = tkinter.Label(root, text="Youtube Video Downloader")
 title.pack()
 
+entry1 = tkinter.Entry(root)#, bd=5)
+entry1.pack()
+
+button1 = tkinter.Button(root, text="Download", fg="white", bg="red", command=download)
+button1.pack()
+
+
+
+root.mainloop()
